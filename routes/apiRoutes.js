055b01632ef183dc9f0028c16feefedd38b45332
aliciachamar <aliciachamar@gmail.com>
2021-07-14 13:03:16 -0600

    added rest of apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,29 @@
-const express = require("express");
-const fs = require("fs");
+const router = require("express").Router();
 const db = require("../db/db.json");
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 
-const router = express.Router();
-
-router.get('/api/notes', (req, res) => {
-    let database = fs.readFileSync('db/db.json');
-    database = JSON.parse(database);
+router.get('/notes', (req, res) => {
+    let database = JSON.parse(fs.readFileSync('db/db.json'));
     res.json(database);
-    console.log("test");
 });
 
-// router.post("/api/notes", (req, res) => {
-//     console.log("test");
-// });
+router.post('/notes', (req, res) => {
+    let database = JSON.parse(fs.readFileSync('db/db.json'));
+    const newNote = {
+        ...req.body, id: uuidv4()
+    };
+    database.push(newNote);
+    fs.writeFileSync('db/db.json', JSON.stringify(database));
+    res.json(database);
+});
 
+router.delete('/notes/:id', (req, res) => {
+    let database =JSON.parse(fs.readFileSync('db/db.json'));
+    let dbFiltered = database.filter(note => note.id !== req.params.id);
+    fs.writeFileSync('db/db.json', JSON.stringify(dbFiltered));
+    res.json(dbFiltered);
+});
+    
+    
 module.exports = router;
